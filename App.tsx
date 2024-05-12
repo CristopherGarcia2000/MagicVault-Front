dfa50fd738a12aa091c14f7c7d998a03dd16ca93
= <=>
2024-05-12 13:04:36 +0100

    fix:"Refactored code"

--- App.tsx
+++ App.tsx
@@ -2,12 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import colors from '../magic-vault-app-front/src/styles/colors';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/home';
-
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
@@ -15,8 +9,6 @@ export default function App() {
       <Text>Bienvenido A Magic Vault</Text>
       <StatusBar style="auto" />
     </View>
-
-    
   );
 }
 
