789e9ecf9714c6252adf8c61330af6472baae725
= <=>
2024-05-12 12:53:46 +0100

    feat:"Basic Home Structure and routing"

--- App.tsx
+++ App.tsx
@@ -2,11 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import colors from '../magic-vault-app-front/src/styles/colors';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/home';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bienvenido A Magic Vault</Text>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
