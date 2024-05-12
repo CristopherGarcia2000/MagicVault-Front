c16e828fb501b855018032d3306eed1491509fca
= <=>
2024-05-12 14:07:15 +0100

    fix:"Colors App.tsx fixed"

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import colors from '../magic-vault-app-front/src/styles/colors';
 import AppNavigator from './src/components/appNavigator';
 
 export default function App() {
