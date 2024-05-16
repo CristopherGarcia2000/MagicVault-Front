e68a575716e9b1b73a328164c072b7edff2e098f
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-05-16 09:17:13 +0000

    feat: Register and Colors Fixed

--- src/screens/colections.tsx
+++ src/screens/colections.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import colors from '../styles/colors'
+import Colors from '../styles/colors'
 
 export default function CollectionsScreen() {
   return (
@@ -15,7 +15,7 @@ export default function CollectionsScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.GreyNeutral,
+    backgroundColor: Colors.GreyNeutral,
     alignItems: 'center',
     justifyContent: 'center',
   },
