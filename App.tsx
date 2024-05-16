347486d021aa20de157af85afdbca70c1320bcbe
Pablo-Vadillo <usr3588@salesianos-lacuesta.net>
2024-05-16 11:50:43 +0000

    feat:"Footer done and Context"

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import AppNavigator from './src/components/appNavigator';
-import TabNavigation from './src/components/TabNavigation';
-import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   
--- src/components/appNavigator.tsx
+++ src/components/appNavigator.tsx
@@ -7,50 +7,63 @@ import RegisterScreen from '../screens/register';
 import SearchScreen from '../screens/search';
 import CollectionsScreen from '../screens/colections';
 import DecksScreen from '../screens/decks';
-import ScannerScreen from '../screens/scanner';
-import TabNavigation from './TabNavigation';
 import colors from '../styles/colors';
 import HeaderLogo from './header';
 import UserProfileScreen from '../screens/userProfile';
 import UserProfileButton from './userProfileButton';
-
+import Footer from './footer';
+import { SelectedScreenProvider, useSelectedScreen } from '../components/context/selectedScreenContext';
 
 const Drawer = createDrawerNavigator();
 
 function AppNavigator() {
-    
-    return (
-        <NavigationContainer>
-            <Drawer.Navigator
-                initialRouteName="Home"
-                screenOptions={{
-                    drawerStyle: {
-                        backgroundColor: colors.GreyNeutral, 
-                    },
-                    drawerActiveTintColor: colors.Gold, 
-                    drawerInactiveTintColor: colors.Beige, 
-                    headerStyle: {
-                        backgroundColor: colors.GreyDark, 
-                    },
-                    headerTintColor: colors.Beige,
-                    headerTitle: () => <HeaderLogo/>,
-                    headerTitleAlign: 'center',
-                    headerRight: () => <UserProfileButton/>
-                }}
-            >
-                <Drawer.Screen name="TabNavigator" component={TabNavigation} />
-                <Drawer.Screen name="Home" component={HomeScreen} />
-                <Drawer.Screen name="Login" component={LoginScreen} />
-                <Drawer.Screen name="Register" component={RegisterScreen} />
-                <Drawer.Screen name="Search" component={SearchScreen} />
-                <Drawer.Screen name="Collections" component={CollectionsScreen} />
-                <Drawer.Screen name="Decks" component={DecksScreen} />
-                <Drawer.Screen name="Scanner" component={ScannerScreen} />
-                <Drawer.Screen name="UserProfile" component={UserProfileScreen} />          
+  const { setSelectedScreen } = useSelectedScreen();
 
-            </Drawer.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: colors.GreyNeutral, 
+          },
+          drawerActiveTintColor: colors.Gold, 
+          drawerInactiveTintColor: colors.Beige, 
+          headerStyle: {
+            backgroundColor: colors.GreyDark, 
+          },
+          headerTintColor: colors.Beige,
+          headerTitle: () => <HeaderLogo/>,
+          headerTitleAlign: 'center',
+          headerRight: () => <UserProfileButton/>
+        }}
+        screenListeners={({ route }) => ({
+          state: (e) => {
+            const currentRoute = e.data.state.routes[e.data.state.index];
+            setSelectedScreen(currentRoute.name);
+          }
+        })}
+      >
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Login" component={LoginScreen} />
+        <Drawer.Screen name="Register" component={RegisterScreen} />
+        <Drawer.Screen name="Search" component={SearchScreen} />
+        <Drawer.Screen name="Collections" component={CollectionsScreen} />
+        <Drawer.Screen name="Decks" component={DecksScreen} />
+        <Drawer.Screen name="UserProfile" component={UserProfileScreen}
+          options={{
+            drawerItemStyle: { display: 'none' }
+          }} />   
+      </Drawer.Navigator>
+      <Footer />
+    </NavigationContainer>
+  );
 }
 
-export default AppNavigator;
+const App = () => (
+  <SelectedScreenProvider>
+    <AppNavigator />
+  </SelectedScreenProvider>
+);
+
+export default App;
